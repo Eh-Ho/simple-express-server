e8b9e69e2eaabadf4477e68f6cf26d554d17134a
Ehsan Hosseini <ehsan@Ehsans-MacBook-Air.local>
2025-10-25 12:54:00 +0330

    date middleware

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const logger = require('./middlewares/logger_middleware')
+const date = require('./middlewares/date_middleware')
+
 const app = express();
 
+app.use(date)
 app.use(logger)
 
 
