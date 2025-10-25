401c21833005ef3071c996f06d924a05388e9855
Ehsan Hosseini <ehsan@Ehsans-MacBook-Air.local>
2025-10-25 18:24:59 +0330

    file rendering in public routes

--- routes/public_routes.js
+++ routes/public_routes.js
@@ -1,9 +1,21 @@
 const express = require ('express');
-const router = express.Router();
+const path = require('path')
 
-router.route('/').get((req, res)=>{
-    res.send('get request')
-}).post((req, res)=>{
+
+const router = express.Router();
+router.route('/').get((req, res, next)=>{
+    const filename = 'index.html'
+    const options ={
+        root : path.join(process.cwd(),'web')
+    }
+    res.status(200).sendFile(filename, options, (err)=>{
+        if(err){
+            next(err)
+        }else{
+            console.log(`file ${filename} sent`)
+        }
+    })
+}).post((req, res, next)=>{
     res.send('post request')
 })
 
--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const logger = require('./middlewares/logger_middleware')
 const date = require('./middlewares/date_middleware')
 const router = require('./routes/public_routes');
-
+const path = require('path')
 const app = express();
 
 app.use(date)
 app.use(logger)
-app.use(router)
+app.use(router,express.static(path.join(process.cwd(), 'web')))
 
 
 app.listen(3000,()=>{
