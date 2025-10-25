f33cb1244f0bf2b19033699907bab3ada47832f7
Ehsan Hosseini <ehsan@Ehsans-MacBook-Air.local>
2025-10-25 18:29:27 +0330

    public router var changed

--- routes/public_routes.js
+++ routes/public_routes.js
@@ -2,8 +2,8 @@ const express = require ('express');
 const path = require('path')
 
 
-const router = express.Router();
-router.route('/').get((req, res, next)=>{
+const publicRouter = express.Router();
+publicRouter.route('/').get((req, res, next)=>{
     const filename = 'index.html'
     const options ={
         root : path.join(process.cwd(),'web')
@@ -19,4 +19,4 @@ router.route('/').get((req, res, next)=>{
     res.send('post request')
 })
 
-module.exports = router;
+module.exports = publicRouter;
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const logger = require('./middlewares/logger_middleware')
 const date = require('./middlewares/date_middleware')
-const router = require('./routes/public_routes');
+const publicRouter = require('./routes/public_routes');
 const path = require('path')
 const app = express();
 
 app.use(date)
 app.use(logger)
-app.use(router,express.static(path.join(process.cwd(), 'web')))
+app.use(publicRouter,express.static(path.join(process.cwd(), 'web')))
 
 
 app.listen(3000,()=>{
